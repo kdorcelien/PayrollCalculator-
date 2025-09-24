1109512e62070076204be5d79e82cdec8d5978cd
kdorcelien <kdorcelien.kd@gmail.com>
2025-09-24 15:36:09 -0400

    initial commit

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,7 +1,27 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
     public static void main(String[] args) {
+        Scanner payroll = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String name = payroll.nextLine();
+
+        System.out.print("Please enter hours worked: ");
+        int hours = payroll.nextInt();
+        payroll.nextLine();
+
+        System.out.print("Please enter hourly rate: ");
+        double pay = payroll.nextDouble();
+        payroll.nextLine();
+
+        System.out.printf("%d * %d = %d ", hours, pay,(hours * pay));
+
+
+
+
 
     }
 }
